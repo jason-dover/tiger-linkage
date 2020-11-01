e3c71619db69e6efb101c5dcd3002ef6b475681e
Jason Dover <jason.dover@fundingcircle.com>
2020-11-01 10:52:10 -0800

    Adding initial draft of containerized tigergraph

---
+++ run.sh
@@ -0,0 +1,11 @@
+# "Borrowed" from https://github.com/DavidBakerEffendi/tigergraph/blob/master/3/3.0.5/run.sh
+#!/bin/bash
+
+source ./config/config.conf
+
+docker build -t ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION} --no-cache . || exit 1
+read -r -p "Would you like to push the image? [y/n] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]];
+then
+    docker push ${DOCKER_ACC}/${DOCKER_REPO}:${TG_VERSION}
+fi
